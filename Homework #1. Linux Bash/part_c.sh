5d66f73022b80777ab1dcf66526a32370b682574
Pavlo Abolmasov <100512886+ilovekharkiv@users.noreply.github.com>
2023-01-21 22:01:55 +0200

    Update part_c.sh

--- Homework #1. Linux Bash/part_c.sh
+++ Homework #1. Linux Bash/part_c.sh
@@ -3,7 +3,7 @@
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
 echo "Backup has started"
-rsync -avv $1 $2 --log-file=backup_log.log
+rsync -ar --delete $1 $2 --log-file=$HOME/Desktop/backup_log.log
 echo "Wait a sec, $USER"
 sleep 1
-echo -e "Backup has been finished. Check ~/Desktop/mylog.log \nCurrent time is $TIMESTAMP"
+echo -e "Backup has been finished. Current time is $TIMESTAMP"
